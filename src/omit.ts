18ccd40168d95dec0f6224c820908de9ce835d5c
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2024-03-10 22:10:29 +0900

    feat: add omit

---
+++ src/omit.ts
@@ -0,0 +1,10 @@
+export function omit<T extends Record<string, unknown>>(obj: T): Partial<T> {
+	const newObj: Partial<T> = {};
+
+	for (const key in obj) {
+		if (obj[key] !== undefined) {
+			newObj[key] = obj[key];
+		}
+	}
+	return newObj;
+}
